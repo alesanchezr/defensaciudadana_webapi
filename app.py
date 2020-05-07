59b6b13ff96ef81b9046010cb202b6b2fb84bd25
Guillermo <guillermoPiedrabuena>
2020-05-07 18:38:11 -0400

    the SQLAlchemy create the database, lacks the flask enpoints

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+
+app = Flask(__name__) #instancio la aplicación Flask
+
+#configuraciones de la app Flask
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://defensaciudadana:Guillermo174027447@localhost/defensaciudadana_webapi'
+
+db = SQLAlchemy(app) #instancio el objeto SQLAlchemy, pues estos son instancias de clases de los frameworck
+
+
+class Clients(db.Model):
+    clients_id = db.Column(db.Integer, primary_key=True)
+    clients_name = db.Column(db.String(50), unique=True, nullable=False)
+    clients_rut = db.Column(db.String(16), unique=True, nullable=False)
+    clients_nationality = db.Column(db.String(20), unique=False, nullable=True)
+    clients_civilStatus = db.Column(db.String(30), unique=True, nullable=True)
+    clients_job = db.Column(db.String(30), unique=True, nullable=True)
+    clients_address = db.Column(db.String(100), unique=False, nullable=True)
+    clients_contact = db.Column(db.String(40), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<Clients %r>' % self.clients_id, self.clients_name, self.clients_rut, self.clients_nationality, self.clients_civilStatus, self.clients_job, self.clients_address, self.clients_contact
+
+
+class Cases(db.Model):
+    __tablename__ ='cases'
+    cases_id = db.Column(db.Integer, primary_key=True)
+    cases_description = db.Column(db.String(200), unique=False, nullable=False)
+    cases_rol_rit_ruc = db.Column(db.String(50), unique=False, nullable=True)
+    cases_trial_entity = db.Column(db.String(60), unique=False, nullable=False)
+    cases_legalIssue = db.Column(db.String(20), unique=False, nullable=True)
+    cases_procedure = db.Column(db.String(60), unique=False, nullable=True)
+    cases_objetive = db.Column(db.String(100), unique=False, nullable=False)
+    cases_update = db.Column(db.String(300), unique=False, nullable=True)
+    cases_updateDate = db.Column(db.DateTime, unique=False, nullable = True)
+    cases_activeCase = db.Column(db.Boolean(60), unique=False, nullable=True)
+    cases_incomeDate = db.Column(db.DateTime, unique=False, nullable = False, default=datetime.datetime.utcnow)
+    cases_deadLine = db.Column(db.DateTime, unique=False, nullable = True)
+
+    client_id = db.Column(db.Integer, db.ForeignKey('clients.clients_id'))
+    cases_documents = db.relationship('Document', backref='Case', lazy=True)
+    #lawyers_case = db.relationship('Lawyer', secondary=lawyer_case, lazy='subquery', backref=db.backref('case', lazy=True))
+
+    def __repr__(self):
+       return '<Cases %r>' %  self.cases_id, self.cases_description, self.cases_rol_rit_ruc, self.cases_trial_entity, self.cases_legalIssue, self.cases_procedure, self.cases_objetive, self.cases_update, self.cases_updateDate, self.cases_activeCase, self.cases_incomeDate, self.cases_deadLine, 
+
+
+class Documents(db.Model):
+    __tablename__='documents'
+    documents_id = db.Column(db.Integer, primary_key=True) 
+    documents_type = db.Column(db.String(100), unique=False, nullable=True)
+    documents_date = db.Column(db.DateTime, unique=False, nullable = False, default=datetime.datetime.utcnow)
+
+    case_id = db.Column(db.Integer, db.ForeignKey('cases.cases_id'), nullable=False)
+
+    def __repr__(self):
+        return '<Document %r>' % self.documents_id, self.documents_type, self.documents_date
+
+
+
+class Corporations(db.Model):
+    __tablename__='corporations'
+    corporation_id = db.Column(db.Integer, primary_key=True)
+    corporation_name = db.Column(db.String(200), unique=False, nullable=False)
+    corporation_type = db.Column(db.String(50), unique=False, nullable=True)    
+    corporation_CBR = db.Column(db.String(50), unique=False, nullable=False)
+    corporation_rolSII = db.Column(db.String(16), unique=False, nullable=True)
+    corporation_taxType = db.Column(db.String(15), unique=False, nullable=True)
+    
+    
+
+    def __repr__(self):
+        return '<Message %r>' % self.corporation_id, self.corporation_name, self.corporation_type, self.corporation_CBR, self.corporation_rolSII, self.corporation_taxType
+ 
