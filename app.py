599117c36da1139a2d7ff70e22d4ec71994d98b8
Guillermo <guillermoPiedrabuena>
2020-05-15 13:45:33 -0400

    the cases enpoints laks the put rest method

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ class Cases(db.Model):
     cases_deadLine = db.Column(db.DateTime, unique=False, nullable = True)
 
     #se pone minuscula la tabla clients pues mira directamente a la base de datos y no a la clase de python
-    cases_client_id = db.Column(db.Integer, db.ForeignKey('clients.clients_id'))
+    cases_client_id = db.Column(db.Integer, db.ForeignKey('clients.clients_id'), nullable=False)
     # establece relacion con tabla documents
     cases_relationship_documents = db.relationship('Documents', backref='case_document')
 
@@ -152,37 +152,37 @@ def clients(rut):
         incomingData = request.get_json()
         updateData= Clients.query.filter_by(clients_rut=rut).first()
         if incomingData['name'] != "":
-            updateData.name = "updated!"
+            updateData.name = incomingData['name']
             db.session.commit()
             return "updated"
 
         if incomingData['rut'] != "":
-            updateData.clients_rut = "updated!"
+            updateData.clients_rut = incomingData['rut']
             db.session.commit()
             return "updated"
 
         if incomingData['nationality'] != "":
-            updateData.clients_nationality = "updated!"
+            updateData.clients_nationality = incomingData['nationality']
             db.session.commit()
             return "updated"
         
         if incomingData['civilStatus'] != "":
-            updateData.clients_civilStatus = "updated!"
+            updateData.clients_civilStatus = incomingData['civilStatus'] 
             db.session.commit()
             return "updated"
 
         if incomingData['job'] != "":
-            updateData.clients_job = "updated!"
+            updateData.clients_job = incomingData['job']
             db.session.commit()
             return "updated"
         
         if incomingData['address'] != "":
-            updateData.clients_address = "updated!"
+            updateData.clients_address = incomingData['address'] 
             db.session.commit()
             return "updated"
 
         if incomingData['contact'] != "":
-            updateData.clients_contact = "updated!"
+            updateData.clients_contact = incomingData['contact']
             db.session.commit()
             return "updated"     
         
@@ -192,4 +192,76 @@ def clients(rut):
         db.session.delete(deletedRow)
         db.session.commit()
         return "data deleted",200
+
+
+@app.route('/casos/<string:rut>', methods = ['GET','POST', 'PUT', 'DELETE'])# SE MUESTRA EN EL BUSCADOR DEL CLIENTE
+def cases(rut):
+    if request.method == 'GET':
+        list=[]
+        if rut == "17.402.744-7" or rut == "20.968.696-1":
+            resp = db.session.query(Cases).all() #DEPURAR ESTO, TOMAR RESP Y QUE EL MODEL ARROJE UN OBJ DE TODOS LOS CAMPOS
+            for item in resp:
+                list.append({
+            "cases_id": item.cases_id,
+            "cases_description": item.cases_description,
+            "cases_rol_rit_ruc": item.cases_rol_rit_ruc,
+            "cases_trial_entity": item.cases_trial_entity,
+            "cases_legalIssue": item.cases_legalIssue,
+            "cases_procedure": item.cases_procedure,
+            "cases_objetive": item.cases_objetive,
+            "cases_update": item.cases_update,
+            "cases_updateDate": item.cases_updateDate,
+            "cases_activeCase": item.cases_activeCase,
+            "cases_incomeDate": item.cases_incomeDate,
+            "cases_deadLine": item.cases_deadLine,
+            "cases_client_id": item.cases_client_id
+        })
+            return jsonify({"resp": list}),200
+        else:
+            resp = db.session.query(Clients).filter_by(clients_rut=rut).first()
+            client_id= resp.clients_id
+            cases = db.session.query(Cases).filter_by(cases_client_id=client_id).all()
+
+            for item in cases:
+                list.append({
+            "cases_id": item.cases_id,
+            "cases_description": item.cases_description,
+            "cases_rol_rit_ruc": item.cases_rol_rit_ruc,
+            "cases_trial_entity": item.cases_trial_entity,
+            "cases_legalIssue": item.cases_legalIssue,
+            "cases_procedure": item.cases_procedure,
+            "cases_objetive": item.cases_objetive,
+            "cases_update": item.cases_update,
+            "cases_updateDate": item.cases_updateDate,
+            "cases_activeCase": item.cases_activeCase,
+            "cases_incomeDate": item.cases_incomeDate,
+            "cases_deadLine": item.cases_deadLine,
+            "cases_client_id": item.cases_client_id
+        })
+
+            return jsonify({"resp": list}),200
+
+    if request.method == 'POST':
+        incomingData = request.get_json()
+        insertedData= Cases(cases_description=incomingData['cases_description'], cases_rol_rit_ruc=incomingData['cases_rol_rit_ruc'], cases_trial_entity=incomingData['cases_trial_entity'], cases_legalIssue=incomingData['cases_legalIssue'], cases_procedure=incomingData['cases_procedure'], cases_objetive=incomingData['cases_objetive'], cases_update=incomingData['cases_update'], cases_activeCase=incomingData['cases_activeCase'], cases_client_id=incomingData['cases_client_id'] )
+        db.session.add(insertedData)
+        db.session.commit()
+        return "data inserted",200
+
+    if request.method == 'PUT':
+        incomingData = request.get_json()
+        updateData= Cases.query.filter_by(cases_rol_rit_ruc=incomingData["cases_rol_rit_ruc"]).first()
+        if incomingData['cases_description'] != " ":
+            updateData.cases_description = incomingData['cases_description'] 
+            db.session.commit()
+            return "updated"
+        
+
+    if request.method == 'DELETE':
+        incomingData = request.get_json()
+        deletedRow= Cases.query.filter_by(cases_rol_rit_ruc=incomingData["cases_rol_rit_ruc"]).first()
+        db.session.delete(deletedRow)
+        db.session.commit()
+        return "data deleted",200
+        
         
